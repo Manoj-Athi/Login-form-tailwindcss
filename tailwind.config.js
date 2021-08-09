06ee11137ae92127558da4d9fe5f47da0765e457
manoj kumar <manojkumar@manojs-MacBook-Air.local>
2021-08-09 11:50:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        '40-r': '40rem',
+        '35-r': '35rem',
+        '30-r': '30rem'
+      },
+      height: {
+        '40-r': '40rem',
+        '35-r': '35rem',
+        '30-r': '30rem'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      boxShadow: {
+        '5xl': '20px 20px 50px rgba(0, 0, 0, 0.5)',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
